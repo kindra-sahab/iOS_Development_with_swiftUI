56885c5caa6303bf1d9bf5c2b412e837d953d8af
Jatin Kindra (IND) <jkindra@mafcarrefour.com>
2023-04-10 13:26:17 +0530

    new project created

---
+++ Learning path 1 - iOS and SwiftUI for Beginners/Your first iOS and swiftUI App from Scratch/BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Jatin Kindra on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
